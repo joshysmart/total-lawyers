d48d7157f54e9dd658e6232472dbeb0938c4d299
joshysmart <sanijoshua10#.com>
2024-08-18 13:33:27 +0100

    fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,7 @@ import Link from "next/link";
 export default function Home() {
   return (
     <main className="container_bg bg-white w-full flex min-h-screen flex-col items-center">
-      <div className="container_bg flex-1 bg-blue w-full flex flex-col items-center justify-center p-4 lg:py-16 ">
+      <div className="container_bg flex-1 bg-blue w-full flex flex-col items-center justify-center p-4 py-8 lg:py-16 ">
         <div className="w-full lg:max-w-[890px] flex flex-col gap-8 lg:gap-12 items-center">
           <Link href="/">
             <Image src="/logo.png" alt="Logo" width={200} height={36} />
